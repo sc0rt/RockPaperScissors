0c07d962aa72539c023ed472d9d11edc30fd9259
Scott Garland <scott.garland@ontariotechu.net>
2020-02-06 20:58:13 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = http.Server(app);
 const clientPath = `${__dirname}/../static/`;
 console.log(`Serving static from ${clientPath}`);
 
-app.set('port', 8080);
+app.set('port', 5000);
 app.use('/static', express.static(clientPath));
 
 const io = socketIO(server);
@@ -30,7 +30,7 @@ server.on('error', function(err) {
     console.error('Server error: ', err);
 });
 
-// listen to port 8080 and call this function
-server.listen(8080, function() {
-    console.log('RPS has started on 8080');
+// listen to port 5000 and call this function
+server.listen(5000, function() {
+    console.log('RPS has started on 5000');
 });
